9c193151904c2d64021b9d669869ebc69e1fa3f9
mtsunori <mickmc.3232@i.softbank.jp>
2019-08-25 17:42:49 +0900

    edit_ajax_chat-space

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,57 @@
+$(document).on('turbolinks:load', function(){
+  function buildHTML(message) {
+    var content = message.content ? `${ message.content }` : "";
+    var img = message.image ? `<img src= ${ message.image }>` : "";
+    var html = `<div class="message" data-id="${message.id}">
+                  <div class="upper-message">
+                    <p class="upper-message__user-name">
+                      ${message.user_name}
+                    </p>
+                    <p class="upper-message__date">
+                      ${message.date}
+                    </p>
+                  </div>
+                  <div class="lower-message">
+                    <p class= "lower-message__content">
+                    ${content}
+                    </p>
+                    ${img}
+                  </div>
+                </div>`
+  return html;
+  }
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var message = new FormData(this);
+    var url = $(this).attr('action');
+    $.ajax({  
+      url: url,
+      type: 'POST',
+      data: message,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      console.log('success!'); 
+      console.log(message);
+      var html = buildHTML(data);
+      $('.messages').append(html);
+      $('.form__message').val('');
+      $('.messages').animate({scrollTop: $('.messages').get(0).scrollHeight})
+    })
+    .fail(function(data){
+      alert('エラーが発生したためメッセージは送信できませんでした。');
+    })
+    .always(function(data){
+      $('.form__submit').prop('disabled', false);
+    })
+})
+})
+
+
+
+//   $('.messages').animate({
+//     scrollTop: position
+//   }, 300, 'swing');
+// }
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -10,12 +10,17 @@
   def create
     @message = @group.messages.new(message_params)
     if @message.save
-      redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'
+      # redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'
+      respond_to do |format|
+        format.html { redirect_to group_messages_path(params[:group_id])}
+        format.json
+      end 
     else
       @messages = @group.messages.includes(:user)
       flash.now[:alert] = 'メッセージを入力してください。'
       render :index
     end
+    
   end
 
   private
@@ -28,3 +33,13 @@
     @group = Group.find(params[:group_id])
   end
 end
+
+# def create
+#   @message = @group.messages.new(message_params)
+#   if @message.save
+#     redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'
+#   else
+#     @messages = @group.messages.includes(:user)
+#     flash.now[:alert] = 'メッセージを入力してください。'
+#     render :index
+#   end
