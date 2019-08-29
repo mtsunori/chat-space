fa9a4402e30c219d4d4480fcd0e4f7afa531fa7e
mtsunori <mickmc.3232@i.softbank.jp>
2019-08-29 10:51:18 +0900

    edit_auto_chat_space

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,24 +1,25 @@
+
 $(document).on('turbolinks:load', function(){
+  
   function buildHTML(message) {
-    var content = message.content ? `${ message.content }` : "";
-    var img = message.image ? `<img src= ${ message.image }>` : "";
-    var html = `<div class="message" data-id="${message.id}">
-                  <div class="upper-message">
-                    <p class="upper-message__user-name">
-                      ${message.user_name}
-                    </p>
-                    <p class="upper-message__date">
-                      ${message.date}
-                    </p>
-                  </div>
-                  <div class="lower-message">
-                    <p class= "lower-message__content">
-                    ${content}
-                    </p>
-                    ${img}
-                  </div>
-                </div>`
-  return html;
+    image = (message.image) ? `<img class= "lower-message__image" src=${message.image} >` : ""; 
+    var html = `<div class="message" data-message-id="${message.id}"> 
+          <div class="upper-message">
+            <div class="upper-message__user-name">
+              ${message.user_name}
+            </div>
+            <div class="upper-message__date">
+              ${message.date}
+            </div>
+          </div>
+          <div class="lower-message">
+            <p class="lower-message__content">
+              ${message.content}
+            </p>
+              ${image}
+          </div>
+        </div>`
+    return html;
   }
   $('#new_message').on('submit', function(e){
     e.preventDefault();
@@ -42,5 +43,30 @@ $(document).on('turbolinks:load', function(){
     .fail(function(data){
       alert('エラーが発生したためメッセージは送信できませんでした。');
     })
-})
-})
+
+  })
+  var reloadMessages = function() {
+    if (window.location.href.match(/\/groups\/\d+\/messages/)){
+      var last_message_id = $('.message:last').data("message-id");
+      $.ajax({
+        url: "api/messages",
+        type: 'get',
+        dataType: 'json',
+        data: {last_id: last_message_id}
+      })
+      .done(function(messages) {
+        var insertHTML = '';
+        messages.forEach(function (message) {
+          insertHTML = buildHTML(message);
+          $('.messages').append(insertHTML);
+        })
+        $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+      })
+      .fail(function() {
+        alert('自動更新に失敗しました');
+      });
+    }
+  };
+  setInterval(reloadMessages, 5000);
+});
+
