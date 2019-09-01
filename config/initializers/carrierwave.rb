927de35a6c8646e0144e2c02deb7512eaeadd26d
mtsunori <mickmc.3232@i.softbank.jp>
2019-09-01 20:36:28 +0900

    入力もれ訂正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -13,5 +13,5 @@ CarrierWave.configure do |config|
   }
 
   config.fog_directory  = 'test-upload-mitsunori'
-  config.asset_host = 'https://ap-northeast-1.amazonaws.com/test-upload-mitsunori'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/test-upload-mitsunori'
 end
